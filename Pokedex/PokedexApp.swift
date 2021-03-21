df7249df37c9b027f307bc2434dc7bb04d40dfd0
Bruce Rick <brucericktgs@gmail.com>
2021-03-21 17:28:08 -0400

    Initial commit of Xcode project

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Bruce Rick on 2021-03-21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
