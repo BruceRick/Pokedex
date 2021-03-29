24c9e322430392562856c70a590b02e5a01e496d
Bruce Rick <brucericktgs@gmail.com>
2021-03-28 22:35:41 -0400

    Cleaned up pokemon details view logic

---
+++ Pokedex/Source/Logic/ImageLoader.swift
@@ -0,0 +1,30 @@
+//
+//  ImageLoader.swift
+//  Pokedex
+//
+//  Created by Bruce Rick on 2021-03-28.
+//
+
+import Foundation
+import Combine
+
+class ImageLoader: ObservableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+
+    init(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
+
---
+++ Pokedex/Source/Views/PokemonDetails.swift
@@ -0,0 +1,59 @@
+//
+//  PokemonDetails.swift
+//  Pokedex
+//
+//  Created by Bruce Rick on 2021-03-22.
+//
+
+import SwiftUI
+import Combine
+
+struct PokemonDetails: View {
+  let pokemonName: String
+  @State private var pokemon: API.Pokemon? { didSet { didSetPokemon() } }
+  @State private var types: [PokemonType] = []
+  @State private var cancellable: AnyCancellable?
+  @State private var imageLoader = ImageLoader(urlString: "")
+  @State var sprite: UIImage = UIImage()
+
+  struct PokemonType: Identifiable {
+    var name: String
+    var id: String { name }
+  }
+  
+  var body: some View {
+    VStack {
+      Image(uiImage: sprite)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 300, height: 300)
+        .onReceive(imageLoader.didChange) { data in
+          self.sprite = UIImage(data: data) ?? UIImage()
+        }
+      if let _ = pokemon {
+        ForEach(types) { type in
+          Text(type.name)
+        }
+      }
+      Spacer()
+    }
+    .onAppear {
+      fetchPokemonDetails()
+    }
+  }
+
+  func fetchPokemonDetails() {
+    cancellable = API.Request(endpoint: .pokemon(pokemonName))
+      .dataTaskPublisher()
+      .map { (response: API.Response) -> API.Pokemon in response.data }
+      .replaceError(with: nil)
+      .assign(to: \.pokemon, on: self)
+  }
+
+  func didSetPokemon() {
+    if let pokemon = pokemon {
+      imageLoader = ImageLoader(urlString: pokemon.sprites.frontDefault)
+      types = pokemon.types.map { PokemonType(name: $0.type.name) }
+    }
+  }
+}
